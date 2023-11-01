8be03a08ecb707a3e4e1451cfe4b02a968539413
AliSubhan15 <alisubhan0229@gmail.com>
2023-11-01 11:02:43 +0700

    commit pertama

---
+++ resources/views/mahasiswa.blade.php
@@ -0,0 +1,5 @@
+<div class="font-mono bg-white dark:bg-slate-800 rounded-lg  px-6 py-8 ring-1 ring-slate-900/5"> 
+ <div class="grid grid-cols-3 gap-4"> 
+ <div class="bg-indigo-300 text-center text-white py-6  rounded-lg">01</div> 
+ <div class="bg-indigo-300 text-center text-white py-6  rounded-lg">02</div>
+
